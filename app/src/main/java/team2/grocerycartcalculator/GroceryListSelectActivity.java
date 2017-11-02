30a95d4d16b1f26c8b1a81cfbca251cd5a07df34
Abahu <diem.carpe.rs@gmail.com>
2017-11-01 22:56:08 -0700

    Calendar interaction, budget display
    Added a date change listener to the Calendar. This starts a new activity
     which in the future should display a list of grocery lists.
    Also added the budget text field over the calendar. This should update
     in real time

---
+++ app/src/main/java/team2/grocerycartcalculator/GroceryListSelectActivity.java
@@ -0,0 +1,13 @@
+package team2.grocerycartcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroceryListSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grocery_list_select);
+    }
+}
