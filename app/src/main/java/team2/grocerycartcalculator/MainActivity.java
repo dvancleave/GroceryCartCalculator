625c45eb2b7e4ce37e6251c2a2a546874f4e1040
CharlesPlachno <cplachno@uoregon.edu>
2017-11-02 17:22:41 -0700

    Added some layout files (added list_view) re arranged calendar view

--- app/src/main/java/team2/grocerycartcalculator/MainActivity.java
+++ app/src/main/java/team2/grocerycartcalculator/MainActivity.java
@@ -3,10 +3,11 @@ package team2.grocerycartcalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     CalendarView calendar;
-
+    ListView listsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
