7cec01d5ab5420501c9b2baa484f3018e365b740
CharlesPlachno <cplachno@uoregon.edu>
2017-11-02 17:00:11 -0700

    Added some layout files (list_picker will probably be deleted since we are keeping the list view on the main calendar view) re arranged calendar view

--- app/src/main/java/team2/grocerycartcalculator/MainActivity.java
+++ app/src/main/java/team2/grocerycartcalculator/MainActivity.java
@@ -2,12 +2,19 @@ package team2.grocerycartcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendar = (CalendarView) findViewById(R.id.calendarView);
+
     }
+
+
 }
