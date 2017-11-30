787c0cbda9533475739976d4b2f2c1b039751ded
CharlesPlachno <cplachno@uoregon.edu>
2017-11-29 18:28:05 -0800

    Added code to checkout

--- app/src/main/java/team2/grocerycartcalculator/CheckoutActivity.java
+++ app/src/main/java/team2/grocerycartcalculator/CheckoutActivity.java
@@ -5,16 +5,46 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import team2.grocerycartcalculator.db.Database;
+import team2.grocerycartcalculator.db.Food;
+import team2.grocerycartcalculator.db.GroceryList;
+
 public class CheckoutActivity extends AppCompatActivity {
 
     private ListView listView;
     private Button finishButton;
+    int groceryListID;
+    GroceryList groceryList;
+    Database database;
+    ArrayList<List_Item> foodList;
+    SwipableListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        database = StartLoadActivity.database;
         setContentView(R.layout.activity_checkout);
         listView = findViewById(R.id.checkout_listview);
         finishButton = findViewById(R.id.checkout_finish_button);
+        groceryListID = getIntent().getIntExtra(MainActivity.LA_INTENT_EXTRA, -2);
+        groceryList = database.getGroceryListByID(groceryListID);
+
+        foodList = new ArrayList<List_Item>();
+        populateFoodList();
+        //creating and setting adapter
+        adapter = new SwipableListAdapter(this, foodList);
+        listView.setAdapter(adapter);
+    }
+
+    private void populateFoodList(){
+        Map<Food, Integer> listmap =  groceryList.getFoodQuantities();
+        ArrayList<Food> foods = new ArrayList<>(listmap.keySet());
+        for(int i=0; i<foods.size(); i++){
+            foodList.add(new List_Item(foods.get(i), listmap.get(foods.get(i))));
+        }
+
     }
 }
