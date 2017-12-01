cec2d913ba0fd197f41865aa81037c23fa107e0f
Abahu <diem.carpe.rs@gmail.com>
2017-12-01 02:25:39 -0800

    Budget setting basics

--- app/src/main/java/team2/grocerycartcalculator/SettingsActivity.java
+++ app/src/main/java/team2/grocerycartcalculator/SettingsActivity.java
@@ -3,16 +3,39 @@ package team2.grocerycartcalculator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+import team2.grocerycartcalculator.db.Budget;
 
 public class SettingsActivity extends AppCompatActivity {
 
     // Used to get our budget. This is the date for the current month
     long date;
+    Budget currBudget;
+    EditText budgetField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         Intent intent = getIntent();
         date = intent.getLongExtra(MainActivity.GLSA_INTENT_EXTRA, -1);
+        currBudget = StartLoadActivity.database.getBudget(date);
+        if(currBudget == null)
+            System.out.println("Budget hasn't been set. What do??");
+        budgetField = findViewById(R.id.settingsBudget);
+        budgetField.setText(String.format(Locale.US, "%.2f", (double)(currBudget.getTotal()) / 100));
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        String budgetString = budgetField.getText().toString();
+        double budget = currBudget.getTotal();
+        if(!budgetString.isEmpty() && !budgetString.equals("."))
+            budget = Double.valueOf(budgetString);
+        currBudget.setTotal((int)(budget * 100));
     }
 }
